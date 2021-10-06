21c2a60a7d66a090a9ee5b240d9b75dd8359163e
rendzina <s.hallett@cranfield.ac.uk>
2021-10-07 00:43:22 +0100

    Update hello_world_docker.py

--- hello_world_docker.py
+++ hello_world_docker.py
@@ -1,1 +1,5 @@
+# This is just a simple line of code to show how to run Python in Docker - with and without bind mounting
+# Edit the file here in both cases to see that:
+# without bind mounting - only the original code can be run - no editing is possible
+# with bind mounting - the source code can be edited and re-run in the docker container
 print("Hello World Docker")
